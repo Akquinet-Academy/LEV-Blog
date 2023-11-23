98dbe413de68567ddda3b09a2825be23f1b23163
VivianeNeubert <viviane.neubert@web.de>
2023-11-23 09:26:33 +0100

    add spring base

---
+++ levblog/src/test/java/com/example/levblog/LevblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.levblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
