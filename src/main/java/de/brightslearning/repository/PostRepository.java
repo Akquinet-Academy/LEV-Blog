0c136ed7f233830886e691346c4f26d511fbca38
VivianeNeubert <viviane.neubert@web.de>
2023-11-24 11:23:40 +0100

    add repositories

---
+++ src/main/java/de/brightslearning/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package de.brightslearning.repository;
+
+import de.brightslearning.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+}
