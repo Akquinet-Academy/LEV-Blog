9de62793817596ac7cf6c13a35fc01b98c45c447
letsc0d3 <emreakca@web.de>
2023-11-28 11:10:39 +0100

    Fix session

--- src/main/java/de/brightslearning/controller/SessionController.java
+++ src/main/java/de/brightslearning/controller/SessionController.java
@@ -1,5 +1,6 @@
 package de.brightslearning.controller;
 
+import de.brightslearning.entity.Session;
 import de.brightslearning.repository.SessionRepository;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,18 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SessionController {
 
-   /* private final SessionRepository sessionRepository;
+   private final SessionRepository sessionRepository;
 
     public SessionController(SessionRepository sessionRepository) {
         this.sessionRepository = sessionRepository;
     }
 
-    @GetMapping("/")
-    public String home(HttpServletResponse response, Model model)
-    {
-        final String food = "Pizza";
-        Cookie newCookie = new Cookie("food", food);
-        response.addCookie(newCookie);
-        return "home";
-    }*/
+
+
+//    @GetMapping("/")
+//    public String home(HttpServletResponse response, Model model)
+//    {
+//        final String food = "Pizza";
+//        Cookie newCookie = new Cookie("food", food);
+//        response.addCookie(newCookie);
+//        return "home";
+//    }
 }
